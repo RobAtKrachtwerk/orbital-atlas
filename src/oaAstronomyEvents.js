6a2e10b80a0e6a1fa5e4bcc836c52a79d532d403
SparklesRob <rob@sparkles.io>
2025-02-16 21:36:25 +0100

    Nieuwe update

--- src/oaAstronomyEvents.js
+++ src/oaAstronomyEvents.js
@@ -11,7 +11,7 @@ const AstronomyEventsCalendar = () => {
     useEffect(() => {
         const fetchEvents = async () => {
             try {
-                const response = await fetch("http://localhost:5000/api/events"); // Call our backend
+                const response = await fetch("https://orbital-atlas.onrender.com/api/events"); // Call our backend
                 if (!response.ok) throw new Error("Failed to fetch events");
 
                 const data = await response.json();
--- src/oaLogin.js
+++ src/oaLogin.js
@@ -8,7 +8,7 @@ function Login({ setIsLoggedIn }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("https://orbital-atlas.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
